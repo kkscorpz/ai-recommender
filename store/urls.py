27e541ddd58b8caf165074edddf503e3e3052483
Katelyn Ebuiza <katelyn3161127@gmail.com>
2025-04-18 17:59:38 +0800

    Initial commit: Set Up Django project with MongoDB and AI recommender

---
+++ store/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('product-list/', views.product_list, name='product_list'),
+    path('add/', views.add_products, name='add_product'),
+    # Other paths...
+]
