5998065a6babebe969e10655d36e2a27c970d86e
Katelyn Ebuiza <katelyn3161127@gmail.com>
2025-04-23 21:56:31 +0800

    Your commit message

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('product-list/', views.product_list, name='product_list'),
     path('add/', views.add_products, name='add_product'),
-    # Other paths...
+   
 ]
